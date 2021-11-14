e6e3cea65fa8cb4beeb68e302c69aa4889d14b51
sekiskylink <sekiskylink@gmail.com>
2021-11-14 23:50:40 +0300

    Add some data to begin with on integrating epivac with the national eidsr for Uganda

---
+++ data.sql
@@ -0,0 +1,4 @@
+INSERT INTO dhis2_instance_pair (source, destination, source_url, source_username, source_password) 
+    VALUES 
+    ('epivac', 'eidsr', 'https://epivac.health.go.ug/api/dataValueSets?', 'foo', 'bar'),
+    ('demis', 'eidsr', 'https://localhost/api/dataValueSets?', 'foo', 'bar');
