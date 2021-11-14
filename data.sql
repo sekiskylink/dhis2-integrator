ebe082f729a8ee1bf8e036e2d1f15c7d0d84bddf
sekiskylink <sekiskylink@gmail.com>
2021-11-15 01:12:40 +0300

    fix sql scripts for data

--- data.sql
+++ data.sql
@@ -2,3 +2,8 @@ INSERT INTO dhis2_instance_pair (source, destination, source_url, source_usernam
     VALUES 
     ('epivac', 'eidsr', 'https://epivac.health.go.ug/api/dataValueSets?', 'foo', 'bar'),
     ('demis', 'eidsr', 'https://localhost/api/dataValueSets?', 'foo', 'bar');
+
+INSERT INTO sync_datasets 
+    (instance_pair_id, dataset_id, dataset_name, reporting_frequency, include_deleted)
+    VALUES
+    (1, 'nTlQefWKMmb', 'Daily Vaccination Summary (V1.2)', 'daily', FALSE);
