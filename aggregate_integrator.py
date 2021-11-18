545be983a430219299c763f00c7112fd7882099e
sekiskylink <sekiskylink@gmail.com>
2021-11-18 18:30:16 +0300

    add queuing to dispatcher2 after payload is created

--- aggregate_integrator.py
+++ aggregate_integrator.py
@@ -229,7 +229,20 @@ for pair in instance_pairs:
                                     'categoryOptionCombo': i['categoryOptionCombo']
                                 } for i in dataValuesTotals.values()]
                         }
+                        extra_params = {
+                            'year': year,
+                            'month': month,
+                            'source': pair['source'],
+                            'destination': pair['destination'],
+                            'facility': district['dhis2_name'],
+                            'is_qparams': "f",
+                            'report_type': '{0}_{1}'.format(pair['source'], pair['destination'])
+                        }
                         print(">>>>>> Period: {0} =====> {1}".format(period, payload))
+                        if DIRECT_SENDING:
+                            pass
+                        else:
+                        queue_in_dispatcher2(json.dumps(payload), ctype="json", params=extra_params)
                 start_date += delta
 
 conn.close()
